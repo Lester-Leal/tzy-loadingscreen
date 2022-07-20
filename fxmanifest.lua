e7342432bd95c4d06d5b69d7fce8fb57207cbc86
Lester <lesterlealyt12@gmail.com>
2022-07-21 01:07:17 +0800

    initial updates

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'TZY DEV'
+description 'Simple Modern FiveM Loading Screen'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+loadscreen_manual_shutdown "yes"
+
+files {
+    'index.html',
+    './css/*.css',
+    './images/*.jpg',
+    './music/music.mp3',
+}
